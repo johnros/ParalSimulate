6098ac2577a8f88dced48516cd36fce2af3ee949
Jonathan Rosenblatt <john.ros.work@gmail.com>
2015-01-07 23:58:12 +0200

    fresh package

---
+++ inProgress.R
@@ -0,0 +1,16 @@
+
+# Return a list with all simulation configurations
+makeConfiguration <- function(replications){
+  
+}
+## Testing:
+.replications <- 1e1
+.m <- c(10,100)
+.p <- 1e2
+.n <- c(1e2,1e3)
+.fitter <- 
+  
+  
+  .configuration <- makeConfiguration()
+
+excludeConfigurations <- function()
