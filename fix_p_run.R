437f8303bc76bd00df31664c831920269cdda09b
Jonathan Rosenblatt <john.ros@gmail.com>
2015-01-21 11:23:30 +0200

    mend

--- fix_p_run.R
+++ fix_p_run.R
@@ -12,21 +12,6 @@ configurations.0 <- makeConfiguration(
   model = my.ols, link = identity, sigma = .sigma, 
   beta.maker = makeBetasDeterministic, beta.star.maker = BetaStarIdentity,
   data.maker=makeRegressionData) 
-
-cl <- makeCluster(min(30, nrow(configurations.0)))
-clusterEvalQ(cl, library(InformationAndInference))
-MSEs.0 <- parApply(cl, configurations.0, 1, replicateMSE)
-
-MSEs.framed.0 <- frameMSEs(MSEs.0, configurations.0)
-plotMSEs(MSEs.framed.0, 'test', robust=TRUE, legend.position='right')
-
-
-
-save(MSEs.0, configurations.0, file='RData/MSEs.0.RData')
-
-
-
-
 ## Ridge
 configurations.1 <- makeConfiguration(
   reps = 5e1,
@@ -37,18 +22,6 @@ configurations.1 <- makeConfiguration(
   model = my.ridge, link = identity, sigma = .sigma, 
   beta.maker = makeBetasDeterministic, beta.star.maker = BetaStarRidge,
   data.maker=makeRegressionData) 
-
-MSEs.1 <- parApply(cl, configurations.1, 1, replicateMSE)
-
-
-MSEs.framed.1 <- frameMSEs(MSEs.1, configurations.1)
-plotMSEs(MSEs.framed.1, 'test', robust=TRUE, legend.position='right')
-
-
-save(MSEs.1, configurations.1, file='RData/MSEs.1.RData')
-
-
-
 ## Non Linear
 configurations.3 <- makeConfiguration(
   reps = 5e1, 
@@ -59,19 +32,6 @@ configurations.3 <- makeConfiguration(
   model = my.log.link, link = exp, sigma = 1, 
   beta.maker = makeBetasRandom, beta.star.maker = BetaStarIdentity, 
   data.maker=makeRegressionData) 
-
-MSEs.3 <- parApply(cl, configurations.3, 1, replicateMSE)
-
-
-MSEs.framed.3 <- frameMSEs(MSEs.3, configurations.3)
-MSEs.framed.3 %>% select(n, average, std.dev, median, mad) %>% head
-plotMSEs(MSEs.framed.3, 'test', robust=TRUE, legend.position='right')
-
-save(MSEs.3, configurations.3, file='RData/MSEs.3.RData')
-
-
-
-
 ## Logistic Regression
 configurations.4<- makeConfiguration(
   reps = 1e2, 
@@ -82,14 +42,29 @@ configurations.4<- makeConfiguration(
   beta.maker = makeBetasRandom, beta.star.maker = BetaStarIdentity, 
   data.maker = makeClassificationData) 
 
-MSEs.4 <- parApply(cl, configurations.4, 1, replicateMSE)
+configurations <- rbind(
+  configurations.0, configurations.1, 
+  configurations.2, configurations.3, configurations.4)
 
-MSEs.framed.4 <- frameMSEs(MSEs.4, configurations.4)
-plotMSEs(MSEs.framed.4, 'test', robust=TRUE, legend.position='right')
-
-save(MSEs.4, configurations.4, file='RData/MSEs.4.RData')
-
-
+cl <- makeCluster(30)
+clusterEvalQ(cl, library(InformationAndInference))
+MSEs <- parApply(cl, configurations, 1, replicateMSE)
 stopCluster(cl)
+save(MSEs, configurations, file='RData/MSEs.RData')
 
+
+##TODO: adapt analysis to unified MSEs object.
+# MSEs.framed.0 <- frameMSEs(MSEs.0, configurations.0)
+# plotMSEs(MSEs.framed.0, 'test', robust=TRUE, legend.position='right')
+# 
+# 
+# MSEs.framed.1 <- frameMSEs(MSEs.1, configurations.1)
+# plotMSEs(MSEs.framed.1, 'test', robust=TRUE, legend.position='right')
+# 
+# MSEs.framed.3 <- frameMSEs(MSEs.3, configurations.3)
+# MSEs.framed.3 %>% select(n, average, std.dev, median, mad) %>% head
+# plotMSEs(MSEs.framed.3, 'test', robust=TRUE, legend.position='right')
+# 
+# MSEs.framed.4 <- frameMSEs(MSEs.4, configurations.4)
+# plotMSEs(MSEs.framed.4, 'test', robust=TRUE, legend.position='right')
 
