0d5454b4d18888c9e00c9d7ef354956e429ec394
Jonathan Rosenblatt <john.ros.work@gmail.com>
2015-02-08 09:48:14 +0200

    detected problem in lm.ridge

--- inProgress.R
+++ inProgress.R
@@ -1,13 +1,18 @@
 # Testing ridge.lm:
 ..p <- 100L
 ..n <- 1e4
-..x <- rexp(..p* ..n)  %>% matrix(ncol=..p, nrow=..n)
+..x <- rnorm(..p* ..n)  %>% matrix(ncol=..p, nrow=..n)
 ..beta <- rnorm(..p,mean = 2)
 ..sigma <- 1e1
 ..y <- ..x %*% ..beta + rnorm(..n, sd = ..sigma)
 
+
+BetaStarRidge <- function(beta, lambda) beta/(1+lambda)
+  
+
 ## Using MASS
-..lambda <- 1e-1
+library(MASS)
+..lambda <- 1e1
 ..beta.star <- BetaStarRidge(beta = ..beta, lambda = ..lambda)
 ..ridge.1 <- lm.ridge(..y~..x-1, lambda=..lambda)
 ..coef.1 <- coef(..ridge.1)
