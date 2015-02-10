521f29c2e100a20f51b87eb86014c1c851b38fed
Jonathan Rosenblatt <john.ros.work@gmail.com>
2015-02-10 19:48:04 +0200

    sync

--- inProgress.R
+++ inProgress.R
@@ -12,7 +12,7 @@ BetaStarRidge <- function(beta, lambda) beta/(1+lambda)
 
 ## Using MASS
 library(MASS)
-..lambda <- 1e1
+..lambda <- 1e0
 ..beta.star <- BetaStarRidge(beta = ..beta, lambda = ..lambda)
 ..ridge.1 <- lm.ridge(..y~..x-1, lambda=..lambda)
 ..coef.1 <- coef(..ridge.1)
@@ -28,3 +28,20 @@ library(ridge)
 plot(..beta~..coef.2);abline(0,1)
 plot(..beta.star~..coef.2);abline(0,1)
 
+# # Using elasticnet
+# library(elasticnet)
+# ..lambda <- 1e1
+# ..beta.star <- BetaStarRidge(beta = ..beta, lambda = ..lambda)
+# ..ridge.3 <- enet(x =..x, y =..y, lambda=..lambda, intercept = FALSE)
+# ..coef.3 <- coef(..ridge.3)
+# plot(..beta~..coef.3);abline(0,1)
+# plot(..beta.star~..coef.3);abline(0,1)
+
+# Using glmnet
+library(glmnet)
+..lambda <- 1e1
+..beta.star <- BetaStarRidge(beta = ..beta, lambda = ..lambda/2)
+..ridge.4 <- glmnet(x =..x, y =..y, family = 'gaussian', alpha = 0, lambda=..lambda, intercept = FALSE)
+..coef.4 <- coef(..ridge.4)@x
+plot(..beta~..coef.4);abline(0,1)
+plot(..beta.star~..coef.4);abline(0,1)
