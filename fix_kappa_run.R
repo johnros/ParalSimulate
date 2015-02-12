f450ef8b91bfde07462396cdbb64291758fa7d93
Jonathan Rosenblatt <john.ros@gmail.com>
2015-02-12 07:03:09 +0000

    prepared fix_kappa to run

--- fix_kappa_run.R
+++ fix_kappa_run.R
@@ -3,10 +3,10 @@ library(InformationAndInference)
 .kappa <- 0.2
 .n <- seq(4e1, 4e3, length.out=6)
 .sigma <- 1e1
-.beta.norm <- 1e1
+.beta.norm <- 1e0
 
 ## OLS
-configurations.00 <- makeConfiguration_fixKappa(
+configurations.00 <- makeConfiguration(
   reps = 1e2, 
   m = .m, 
   n = .n , 
@@ -14,37 +14,41 @@ configurations.00 <- makeConfiguration_fixKappa(
   lambda = NA, 
   sigma = .sigma, 
   model = my.ols, link = identity, beta.maker = makeBetasDeterministic, 
+  beta.norm = .beta.norm,
   beta.star.maker = BetaStarIdentity, data.maker = makeRegressionData,
-  truth.fun=truthOLS, name='ols') 
+  name='ols') 
 ## Ridge
-configurations.10 <- makeConfiguration_fixKappa(
+configurations.10 <- makeConfiguration(
   reps = 1e2,
   m = .m, 
   kappa = .kappa, 
   n = .n, 
   lambda=2,
   model = my.ridge, link = identity, sigma = .sigma, 
-  beta.maker = makeBetasDeterministic, beta.star.maker = BetaStarRidge,
+  beta.maker = makeBetasDeterministic, 
+  beta.norm=.beta.norm, beta.star.maker = BetaStarRidge,
   data.maker=makeRegressionData, name='ridge') 
 ## Non Linear
-configurations.30 <- makeConfiguration_fixKappa(
+configurations.30 <- makeConfiguration(
   reps = 5e2, 
   m = .m, 
   kappa = .kappa, 
   n = .n, 
   lambda = NA, 
   model = my.log.link, link = exp, sigma = 1, 
-  beta.maker = makeBetasRandom, beta.star.maker = BetaStarIdentity, 
+  beta.maker = makeBetasDeterministic, beta.norm=.beta.norm, 
+  beta.star.maker = BetaStarIdentity, 
   data.maker=makeRegressionData, name='nls') 
 ## Logistic Regression
-configurations.40<- makeConfiguration_fixKappa(
+configurations.40<- makeConfiguration(
   reps = 2e2, 
   m = .m, 
   kappa = .kappa, 
   n = .n,
   lambda=NA,
   model = my.logistic, link = sigmoid, sigma = .sigma, 
-  beta.maker = makeBetasRandom, beta.star.maker = BetaStarIdentity, 
+  beta.maker = makeBetasDeterministic, beta.norm=.beta.norm,
+  beta.star.maker = BetaStarIdentity, 
   data.maker = makeClassificationData, name='logistic') 
 
 
@@ -55,11 +59,10 @@ configurations.fix.kappa <- rbind(
   configurations.40)
 
 
-cl <- makeCluster(35)
+cl <- makeCluster(15, type='FORK')
 clusterEvalQ(cl, library(InformationAndInference))
 MSEs.fix.kappa <- parApply(cl, configurations.fix.kappa, 1, replicateMSE)
-stopCluster(cl)
+save(MSEs.fix.kappa, configurations.fix.kappa, file='RData/MSEs_fix_kappa.5.RData')
 
-file.name <- paste('RData/MSEs_fix_kappa.',file.ending,'.RData', sep='')
-save(MSEs.fix.kappa, configurations.fix.kappa, file='RData/MSEs_fix_kappa.1.RData')
+stopCluster(cl)
 ############################################
