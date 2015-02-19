b11cdea80725aaf5c9bcc4e85e4fd37d79bf779d
Jonathan Rosenblatt <john.ros@gmail.com>
2015-02-19 16:12:13 +0200

    fix kappa simulation

--- fix_kappa_run.R
+++ fix_kappa_run.R
@@ -10,7 +10,9 @@ configurations.fix.kappa <- rbind(
 cl <- makeCluster(37, type='FORK')
 clusterEvalQ(cl, library(InformationAndInference))
 MSEs.fix.kappa <- parApply(cl, configurations.fix.kappa, 1, replicateMSE)
-save(MSEs.fix.kappa, configurations.fix.kappa, file='RData/MSEs_fix_kappa.7.RData')
+attr(MSEs.fix.kappa, "createdAt") <- Sys.time()
+
+save(MSEs.fix.kappa, configurations.fix.kappa, file='RData/MSEs_fix_kappa.8.RData')
 
 stopCluster(cl)
 ############################################
