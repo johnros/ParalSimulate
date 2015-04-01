84ef7f1bed51bf8e6d05d33af233a78c2be910f1
Jonathan Rosenblatt <john.ros@gmail.com>
2015-04-01 22:37:12 +0300

    publication parameters

--- ridge_anomalies.R
+++ ridge_anomalies.R
@@ -1,11 +1,6 @@
 # To be run from fix_p_combined_simulations.Rmd
 
-configurations <- rbind(
-  configurations.0, configurations.1, 
-  configurations.3, configurations.4)
-
-
-
+configurations <- configurations.1
 
 # cl <- makeCluster(3, useXDR=TRUE, homogeneous=FALSE, rscript_args = c("--no-init-file", "--no-site-file", "--no-environ"))
 cl <- makeCluster(35, type="FORK", rscript_args = c("--no-init-file", "--no-site-file", "--no-environ"))
@@ -13,7 +8,7 @@ clusterEvalQ(cl, library(InformationAndInference))
 
 MSEs <- parApply(cl, configurations, 1, replicateMSE)
 attr(MSEs, "createdAt") <- Sys.time()
-save(MSEs, configurations, file='RData/MSEs_fix_p.9.RData')
+save(MSEs, configurations, file='RData/MSEs_Ridge_anomalies.1.RData')
 
 stopCluster(cl)
 
